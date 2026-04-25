59ad37230120cbaa038b843c87a4ffc5c7480912
RomanEger <egerromaan@gmail.com>
2026-04-25 13:46:59 +0600

    create UserJpaRepo

---
+++ backend/finance-tracker/infrastructure/src/main/java/com/reger/infrastructure/persistence/UserJpaRepository.java
@@ -0,0 +1,11 @@
+package com.reger.infrastructure.persistence;
+
+import com.reger.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserJpaRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+}
