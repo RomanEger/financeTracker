048eda86357bf2b962d28546f9938c5d383bc761
RomanEger <egerromaan@gmail.com>
2026-04-25 13:25:50 +0600

    create presentation module

---
+++ backend/finance-tracker/presentation/src/main/java/com/reger/presentation/FinanceTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.reger.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FinanceTrackerApplication.class, args);
+    }
+}
---
+++ backend/finance-tracker/presentation/src/test/java/com/reger/presentation/FinanceTrackerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.reger.presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
