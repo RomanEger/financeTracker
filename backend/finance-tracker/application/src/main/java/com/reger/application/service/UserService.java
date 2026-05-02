f4d9164680abe483e221d523e1ebc9c13f2624db
RomanEger <egerromaan@gmail.com>
2026-05-02 13:32:12 +0600

    create user service

---
+++ backend/finance-tracker/application/src/main/java/com/reger/application/service/UserService.java
@@ -0,0 +1,38 @@
+package com.reger.application.service;
+
+import com.reger.application.dto.RegisterRequest;
+import com.reger.application.dto.UserResponse;
+import com.reger.domain.entity.User;
+import com.reger.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserResponse register(RegisterRequest registerRequest) {
+        if (userRepository.existsByUsername(registerRequest.userName())) {
+            throw new IllegalStateException("Username is already taken: " + registerRequest.userName());
+        }
+
+        var user = User.create(
+                registerRequest.userName(),
+                passwordEncoder.encode(registerRequest.password())
+        );
+
+        var savedUser = userRepository.create(user);
+
+        return new UserResponse(savedUser.getId(), savedUser.getUsername());
+    }
+
+    public User findByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found: " + username));
+    }
+}
