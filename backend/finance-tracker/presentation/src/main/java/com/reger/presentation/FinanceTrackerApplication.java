3ea7bb99a8b6fad7c4f18996828fd456adc3b951
RomanEger <egerromaan@gmail.com>
2026-05-02 13:39:34 +0600

    add application and infra dependencies

--- backend/finance-tracker/presentation/src/main/java/com/reger/presentation/FinanceTrackerApplication.java
+++ backend/finance-tracker/presentation/src/main/java/com/reger/presentation/FinanceTrackerApplication.java
@@ -3,10 +3,14 @@ package com.reger.presentation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.reger.presentation",
+        "com.reger.application",
+        "com.reger.infrastructure"
+})
 public class FinanceTrackerApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(FinanceTrackerApplication.class, args);
     }
 }
