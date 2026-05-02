153a975bc52e573b3ac9b4c6f0cc3cc137a254dc
RomanEger <egerromaan@gmail.com>
2026-05-02 13:07:23 +0600

    implement user repo

---
+++ backend/finance-tracker/infrastructure/src/main/java/com/reger/infrastructure/persistence/UserRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.reger.infrastructure.persistence;
+
+import com.reger.domain.entity.User;
+import com.reger.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+
+    private final UserJpaRepository userJpaRepository;
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return userJpaRepository.existsByUsername(username);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userJpaRepository.findByUsername(username);
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return userJpaRepository.findById(id);
+    }
+
+    @Override
+    public User create(User user) {
+        return userJpaRepository.save(user);
+    }
+
+    @Override
+    public User update(User user) {
+        return userJpaRepository.save(user);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        userJpaRepository.deleteById(id);
+    }
+}
