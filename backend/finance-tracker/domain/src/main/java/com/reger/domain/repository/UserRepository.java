e2dc459931f9edb40438828ce02136e394daba48
RomanEger <egerromaan@gmail.com>
2026-05-02 13:33:56 +0600

    create UserRepo interface

---
+++ backend/finance-tracker/domain/src/main/java/com/reger/domain/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.reger.domain.repository;
+
+import com.reger.domain.entity.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+    boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findById(UUID id);
+
+    User create(User user);
+
+    User update(User user);
+
+    void delete(UUID id);
+}
