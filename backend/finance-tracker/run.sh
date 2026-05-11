cc16915a23db786d2ed8884e50c2458abc8ca1c9
RomanEger <egerromaan@gmail.com>
2026-05-11 12:30:16 +0600

    Update runner script: add args for debug attach

--- backend/finance-tracker/run.sh
+++ backend/finance-tracker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd "$(dirname "$0")/presentation" && mvn spring-boot:run
+cd "$(dirname "$0")/presentation" && mvn spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
