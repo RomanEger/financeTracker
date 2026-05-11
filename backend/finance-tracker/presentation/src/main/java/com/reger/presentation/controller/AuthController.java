99821d2ca4c648a85e814d3fb106799d43ea7d24
RomanEger <egerromaan@gmail.com>
2026-05-11 17:24:17 +0600

    Create login method

--- backend/finance-tracker/presentation/src/main/java/com/reger/presentation/controller/AuthController.java
+++ backend/finance-tracker/presentation/src/main/java/com/reger/presentation/controller/AuthController.java
@@ -1,12 +1,17 @@
 package com.reger.presentation.controller;
 
+import com.reger.application.dto.LoginRequest;
 import com.reger.application.dto.RegisterRequest;
 import com.reger.application.dto.UserResponse;
 import com.reger.application.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request) {
         var response = userService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> login(
+            @RequestBody LoginRequest request,
+            HttpServletRequest httpServletRequest) {
+        var authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.userName(),
+                        request.password()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        httpServletRequest.getSession(true);
+
+        var userName = authentication.getName();
+        var response = userService.findByUsername(userName);
+
+        return ResponseEntity.ok(response);
     }
 }
