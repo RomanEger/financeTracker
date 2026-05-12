227484134217b15d5713c3547c074654f6897be0
RomanEger <egerromaan@gmail.com>
2026-05-12 09:35:25 +0600

    Create mapper User -> UserResponse

--- backend/finance-tracker/application/src/main/java/com/reger/application/dto/UserResponse.java
+++ backend/finance-tracker/application/src/main/java/com/reger/application/dto/UserResponse.java
@@ -1,6 +1,11 @@
 package com.reger.application.dto;
 
+import com.reger.domain.entity.User;
+
 import java.util.UUID;
 
 public record UserResponse(UUID id, String username) {
+    public static UserResponse from(User user) {
+        return new UserResponse(user.getId(), user.getUsername());
+    }
 }
